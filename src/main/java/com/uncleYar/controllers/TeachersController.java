2a3015f5f8e2d05a68c0b39006119f58ee860bc7
RageAgainstZeds <90nomad@gmail.com>
2018-05-16 00:43:20 +0300

    my brain full of fucks

--- src/main/java/com/uncleYar/controllers/TeachersController.java
+++ src/main/java/com/uncleYar/controllers/TeachersController.java
@@ -9,22 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.uncleYar.entities.Teachers;
 import com.uncleYar.repositories.TeachersRepository;
 
 @Controller
-@RequestMapping("/")
-class HomeController {
-	
+@RequestMapping("/teachers")
+public class TeachersController {
 	@Autowired
 	TeachersRepository teachersRepo;
 	
 	@GetMapping
-	public ModelAndView home() {
+	public ModelAndView teachersPage() {
 		Map<String, String> model = new HashMap<>();
 		
 		model.put("teachersCount", String.valueOf(teachersRepo.count()));
 		
-		return new ModelAndView("index", model);
+		return new ModelAndView("teachers", model);
 	}
-}	
+}
